f33d43c4950d61de86960c7399d364f0890f2c01
Cesar Gutierrez Espitia <ieethczar@gmail.com>
2020-03-09 19:02:07 -0600

    Configuracion y conexion a la base de datos

--- server/index.js
+++ server/index.js
@@ -1,4 +1,17 @@
 const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const MONGO_URI = "mongodb+srv://tester:1234567890p@cluster0-xcaqz.mongodb.net/clone-netflix-db?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const mongo = mongoose.connection;
+
+mongo.on('error', error => console.log(error))
+  .once('open', () => console.log('Connected to database'))
 
 const typeDefs = gql`
     type Query{
